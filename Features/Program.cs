14d1be5f59f050dd44ffb5633f2058b46333a9b6
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-13 23:10:16 -0700

    Create Program Employee objects

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Features
 {
@@ -6,7 +8,16 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott"},
+                new Employee { Id = 2, Name = "Chris"}
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex"} 
+            };
         }
     }
 }
