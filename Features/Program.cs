bf8344f1beffe99a092519cc29bc417c7e8d698d
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-14 23:45:17 -0700

    Change IEnumerables into var

--- Features/Program.cs
+++ Features/Program.cs
@@ -23,13 +23,15 @@ namespace Features
 
             write(square(add(3, 5)));
 
-            IEnumerable<Employee> developers = new Employee[]
+            // IEnumerable<Employee> developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee { Id = 1, Name = "Scott" },
                 new Employee { Id = 2, Name = "Chris" }
             };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            // IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" } 
             };
@@ -46,7 +48,14 @@ namespace Features
             //     Console.WriteLine(employee.Name);
             // }
 
-            foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name))
+            // foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name))
+            // {
+            //     Console.WriteLine(employee.Name);
+            // }
+
+            var query = developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name);
+
+            foreach (var employee in query)
             {
                 Console.WriteLine(employee.Name);
             }
